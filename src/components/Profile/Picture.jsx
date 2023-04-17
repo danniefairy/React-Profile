0feec526cb0eb04b2d7b0760c2850615bdb151ef
Dannie <dannienctu@gmail.com>
2023-04-16 21:07:31 -0700

    first update

--- src/components/Profile/Picture.jsx
+++ src/components/Profile/Picture.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Picture() {
     return (
         <div className="w3-display-container">
-            <img className="profile-img" src="./React-Profile/images/Profile.jpg" alt="Avatar" />
+            <img className="profile-img" src="https://danniefairy.github.io/React-Profile/images/Profile.jpg" alt="Avatar" />
             <br />
             <br />
             <div className="w3-display-bottomleft w3-container w3-text-black">
